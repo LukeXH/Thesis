2b5388ed2ce588af660aa5d70cfd29dcf08f02ea
Lukepahill <luke.pa.hill@gmail.com>
2017-11-01 22:06:08 -0700

    Looking to visualize parallel transport as a ball rolling on a surface.

---
+++ ParallelTransport/rollaball.m
@@ -0,0 +1,8 @@
+clear
+
+% This script attempts to visualize parallel transport on an S^2 sphere as
+% rolling it on the ground along a certain path and then reorienting it so
+% that it's internal frame matches the starting position
+
+
+
